1c5c74d996aaa5afe3f23ab725d52e2f85f07b24
Roman Baukin <rbaukin1984@gmail.com>
2022-08-13 17:53:48 +0400

    feat: add solution lesson #3

--- script.js
+++ script.js
@@ -1,55 +1,78 @@
 /* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+1) Первую часть задания повторить по уроку
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
 
-Проверить, чтобы все работало без ошибок в консоли */
+P.S. Функции вызывать не обязательно*/
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '9'),
-    personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };
+let numberOfFilms;
 
-for (let i = 0; i < 2; i++) {
-    const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-    const scoreLastFilm = prompt('На сколько оцените его?');
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-    if (lastFilm !== null && scoreLastFilm !== null && lastFilm !== '' && scoreLastFilm !== '' && lastFilm.length < 50) {
-        personalMovieDB.movies[lastFilm] = scoreLastFilm;
-    } else {
-        console.log('error');
-        i--;
+    while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms) || numberOfFilms === 0) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
 }
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-    alert('Вы киноман');
-} else {
-    alert('Произошла ошибка');
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+        const scoreLastFilm = prompt('На сколько оцените его?');
+
+        if (lastFilm !== null && scoreLastFilm !== null && lastFilm !== '' &&
+            scoreLastFilm !== '' && lastFilm.length < 50) {
+            personalMovieDB.movies[lastFilm] = scoreLastFilm;
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
 }
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+// rememberMyFilms();
+// detectPersonalLevel();
+showMyDB(personalMovieDB.privat);
+writeYourGenres();
