ded0428b5625779c681d4e7fc2c8c79d892c4df5
Roman Baukin <rbaukin1984@gmail.com>
2022-08-23 13:13:17 +0400

    feat: add solution lesson #44

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const bg = document.querySelector('.promo__bg');
+const promoList = document.querySelectorAll('.promo__interactive-item');
+const promoGenre = bg.querySelector('.promo__genre');
+const advImg = document.querySelectorAll('.promo__adv img');
+
+bg.style.backgroundImage = 'url("../img/bg.jpg")';
+promoGenre.textContent = 'драма';
+
+advImg.forEach((img) => img.remove());
+
+movieDB.movies.sort();
+
+promoList.forEach((movie, i) => movie.textContent = `${i + 1}: ${movieDB.movies[i]}`);
